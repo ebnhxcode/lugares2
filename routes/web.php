4d8d5994727a1efb4aaf373a31f38a1a147f2399
Esteban Ramos Fernandez <esteban.ramos.f@gmail.com>
2018-05-28 05:41:17 -0400

    update/add - clean app.

--- routes/web.php
+++ routes/web.php
@@ -25,30 +25,17 @@ Route::get('/', function () {
 #Primeras rutas
 
 #Rutas que le siguen a la mecanica de ruteos.
-Route::get('/ajax/actividades', 'ActividadController@index_ajax');
-Route::get('/ajax/aplicaciones_accesos', 'AplicacionAccesoController@index_ajax');
-Route::get('/ajax/aplicaciones', 'AplicacionController@index_ajax');
+Route::get('/ajax/roles', 'RoleController@index_ajax');
 Route::get('/ajax/cargos', 'CargoController@index_ajax');
-Route::get('/ajax/clusters', 'ClusterController@index_ajax');
-Route::get('/ajax/datacentros', 'DatacentroController@index_ajax');
-Route::get('/ajax/dominios', 'DominioController@index_ajax');
 Route::get('/ajax/estados', 'EstadoController@index_ajax');
+Route::get('/ajax/permisos', 'PermisoController@index_ajax');
+Route::get('/ajax/usuarios', 'UsuarioController@index_ajax');
 Route::get('/ajax/mantenedores', 'MantenedorController@index_ajax');
 Route::get('/ajax/menus', 'MenuController@index_ajax');
-Route::get('/ajax/permisos', 'PermisoController@index_ajax');
-Route::get('/ajax/roles', 'RoleController@index_ajax');
-Route::get('/ajax/servicios', 'ServicioController@index_ajax');
-Route::get('/ajax/servicios_usuarios', 'ServicioUsuarioController@index_ajax');
-Route::get('/ajax/servidores_accesos', 'ServidorAccesoController@index_ajax');
-Route::get('/ajax/servidores', 'ServidorController@index_ajax');
-Route::get('/ajax/sistemas_operativos', 'SistemaOperativoController@index_ajax');
-Route::get('/ajax/tags', 'TagController@index_ajax');
-Route::get('/ajax/tipos_aplicaciones', 'TipoAplicacionController@index_ajax');
-Route::get('/ajax/tipos_respaldos_discos', 'TipoRespaldoDiscoController@index_ajax');
-Route::get('/ajax/tipos_servidores', 'TipoServidorController@index_ajax');
-Route::get('/ajax/usuarios_bitacora_servicios', 'UsuarioBitacoraServicioController@index_ajax');
-Route::get('/ajax/usuarios', 'UsuarioController@index_ajax');
-Route::get('/ajax/vlans', 'VlanController@index_ajax');
+
+Route::get('/ajax/paises', 'PaisController@index_ajax');
+Route::get('/ajax/regiones', 'RegionController@index_ajax');
+Route::get('/ajax/comunas', 'ComunaController@index_ajax');
 
 
 
@@ -148,27 +135,6 @@ $http_requests = [
  * */
 $modules = [
 
-
-   ['route'=>'servicios','controller'=>'ServicioController'],
-   ['route'=>'servicios_usuarios','controller'=>'ServicioUsuarioController'],
-   ['route'=>'actividades','controller'=>'ActividadController'],
-
-   ['route'=>'aplicaciones','controller'=>'AplicacionController'],
-   ['route'=>'tipos_aplicaciones','controller'=>'TipoAplicacionController'],
-   ['route'=>'tipos_servidores','controller'=>'TipoServidorController'],
-   ['route'=>'tipos_respaldos_discos','controller'=>'TipoRespaldoDiscoController'],
-   ['route'=>'aplicaciones_accesos','controller'=>'AplicacionAccesoController'],
-   ['route'=>'dominios','controller'=>'DominioController'],
-   ['route'=>'servidores','controller'=>'ServidorController'],
-   ['route'=>'servidores_accesos','controller'=>'ServidorAccesoController'],
-   ['route'=>'datacentros','controller'=>'DatacentroController'],
-   ['route'=>'sistemas_operativos','controller'=>'SistemaOperativoController'],
-   ['route'=>'tags','controller'=>'TagController'],
-   ['route'=>'usuarios_bitacora_servicios','controller'=>'UsuarioBitacoraServicioController'],
-   ['route'=>'clusters','controller'=>'ClusterController'],
-   ['route'=>'vlans','controller'=>'VlanController'],
-
-
    ['route'=>'roles', 'controller'=>'RoleController'],
    ['route'=>'cargos','controller'=>'CargoController'],
    ['route'=>'estados','controller'=>'EstadoController'],
@@ -176,6 +142,10 @@ $modules = [
    ['route'=>'usuarios','controller'=>'UsuarioController'],
    ['route'=>'menus','controller'=>'MenuController'],
    ['route'=>'mantenedores','controller'=>'MantenedorController'],
+
+   ['route'=>'paises','controller'=>'PaisController'],
+   ['route'=>'regiones','controller'=>'RegionController'],
+   ['route'=>'comunas','controller'=>'ComunaController'],
 
 ];
 
