f69be0534535fd887df51e31e2a4522b5f91a548
Esteban Ramos Fernandez <esteban.ramos.f@gmail.com>
2018-06-20 17:10:44 -0400

    update/add - let nullable det_dependencia.

--- app/Http/Controllers/DependenciaController.php
+++ app/Http/Controllers/DependenciaController.php
@@ -107,7 +107,7 @@ class DependenciaController extends Controller {
       #Se realiza validacion de los parametros de entrada que vienen desde el formulario
       $this->validacion = Validator::make($request->all(), [
          'nom_dependencia' => "regex:/(^([a-zA-Z0-9_ -áéíóúñÁÉÍÓÚÑ]+)(\d+)?$)/u|required|max:255",
-         'det_dependencia' => "regex:/(^([a-zA-Z0-9_ ,.!@#$%*&-áéíóúñÁÉÍÓÚÑ]+)(\d+)?$)/u|max:255",
+         'det_dependencia' => "nullable|regex:/(^([a-zA-Z0-9_ ,.!@#$%*&-áéíóúñÁÉÍÓÚÑ]+)(\d+)?$)/u|max:255",
       ]);
       #Se valida la respuesta con la salida de la validacion
       if ($this->validacion->fails() == true) {
@@ -143,7 +143,7 @@ class DependenciaController extends Controller {
       $this->validacion = Validator::make($request->all(), [
          'id_dependencia' => 'regex:/(^([0-9]+)(\d+)?$)/u|required|max:255',
          'nom_dependencia' => "regex:/(^([a-zA-Z0-9_ -áéíóúñÁÉÍÓÚÑ]+)(\d+)?$)/u|required|max:255",
-         'det_dependencia' => "regex:/(^([a-zA-Z0-9_ ,.!@#$%*&-áéíóúñÁÉÍÓÚÑ]+)(\d+)?$)/u|max:255",
+         'det_dependencia' => "nullable|regex:/(^([a-zA-Z0-9_ ,.!@#$%*&-áéíóúñÁÉÍÓÚÑ]+)(\d+)?$)/u|max:255",
       ]);
       #Valida si la informacion que se envia para editar al dependencia son iguales los ids
       if ($id != $request["id_$this->nombre_modelo"]) {
